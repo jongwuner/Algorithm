0e719e623b8c0f670f089ea306c94b5c48bf0680
jongwuner <jklh0202@gmail.com>
2018-12-27 21:28:25 +0900

    주석제거

--- UVA/1241.cpp
+++ UVA/1241.cpp
@@ -15,11 +15,11 @@ int main() {
 			scanf("%d", &tmp);
 			nogame[tmp] = true;
 		}
-		for (int i = 1 << N; i >= 1; i = i >> 1) { // 간격
-			for (int j = 0; 1 + j*i <= 1 << N; j++) { // 시작점
+		for (int i = 1 << N; i >= 1; i = i >> 1) { 
+			for (int j = 0; 1 + j*i <= 1 << N; j++) { 
 				bool flag = true;
-				for (int k = 1 + j * i; k < 1 + (j + 1) * i; k++) { // 작은 루프
-					if (nogame[k] == false) { // 이 루프에서 하나라도 게임을 할 경우 더 볼필요도 없다!. 더 작은 단위는 계속 쪼개어 볼테니!
+				for (int k = 1 + j * i; k < 1 + (j + 1) * i; k++) { 
+					if (nogame[k] == false) { 
 						flag = false;
 						break;
 					}
@@ -31,8 +31,8 @@ int main() {
 				}
 				else if(flag){
 					ans++;
-					for (int k = 1 + j * i; k < 1 + (j + 1) * i; k++) { // 작은 루프
-						nogame[k] = false; // 체크했으니 
+					for (int k = 1 + j * i; k < 1 + (j + 1) * i; k++) { 
+						nogame[k] = false; 
 					}
 				}
 			}
