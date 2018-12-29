94d29204a7f9119635cd9f705cd2de5cd9d29081
jongwuner <jklh0202@gmail.com>
2018-12-29 16:41:27 +0900

    트리계속 공부중 20181229

---
+++ BOJ/1068.cpp
@@ -0,0 +1,26 @@
+#include<cstdio>
+int p[50], a, c[50];
+int main()
+{
+	int n, d;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", p + i);
+		if (p[i] >= 0)c[p[i]]++;
+	}
+	scanf("%d", &d);
+	c[p[d]]--;
+	for (int i = 0; i < n; i++)
+	{
+		if (c[i]) continue;
+		int j = i;
+		while (j != -1)
+		{
+			if (j == d) break;
+			j = p[j];
+		}
+		a += j == -1;
+	}
+	printf("%d\n", a);
+}
