ad80b88348be9e26edcb70e92fe5fe5c69010b1b
jongwuner <jklh0202@naver.com>
2019-10-13 07:58:36 +0900

    update boj 12100(2048 easy), 14500(테트로미노)
    # boj 12100
    - 해결완료!
    - 해당내용은 Issue에 담아두었음.
    
    # 14500
    - 해결완료!
    - 해당내용은 Issue에 담아두었음.

--- exercise/BOJ/12100.cpp
+++ exercise/BOJ/12100.cpp
@@ -7,21 +7,40 @@ typedef pair<int, Pair> PPair;
 const int MAX = 25;
 
 int N, ans = 1;
+void print(vector< vector<int> > lis) {
+	printf("*****print*****\n\n");
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			printf("%4d", lis[i][j]);
+		}
+		printf("\n");
+	}
+}
+
 void DFS(int lev, vector<vector<int>> Now) {
-	if (lev >= 5) return;
+	if (lev >= 5) {
+		for (int i = 1; i <= N; i++)
+			for (int j = 1; j <= N; j++)
+				ans = max(ans, Now[i][j]);
+		//print(Now);
+		return;
+	}
 
 	vector<vector<int>> Next;
 
 	// 그대로---------------------------------------------------------
 	Next = Now;
-	for (int i = 1; i <= N; i++)
-		for (int j = 1; j <= N; j++)
-			ans = max(ans, Next[i][j]);
 
 	DFS(lev + 1, Next);
+
+	//printf("☆★ level : %d ☆★\n", lev);
+	//printf("그대로 :: ");
+	//print(Next);
 	Next.clear();
 	//down--------------------------------------------------------------
 	Next = Now;
+
+	/*
 	for (int j = 1; j <= N; j++) {
 		int object = -1; // merge할 object가 없다.
 		for (int i = N; i >= 1; i--) {
@@ -40,10 +59,25 @@ void DFS(int lev, vector<vector<int>> Now) {
 			}
 		}
 	}
+	*/
+	// down - merge
+	for (int j = 1; j <= N; j++) {
+		bool IsPushed[MAX] = { 0 };
+		for (int i = N; i >= 1; i--) {
+			if (Next[i][j] == 0 || IsPushed[i]) continue;
+			for (int k = i - 1; k >= 1; k--) {
+				if (!Next[k][j] || IsPushed[k] || IsPushed[i]) continue;
+				if (Next[i][j] != Next[k][j]) break;
+				Next[i][j] += Next[k][j];
+				Next[k][j] = 0;
+				IsPushed[k] = IsPushed[i] = true;
+			}
+		}
+	}
+
 	// down-mv : 0을 메꿔서 땡긴다!, 0을 잡고 다음 tartget을 여기까지 땡긴다!
 	for (int j = 1; j <= N; j++) {
 		for (int i = N; i >= 1; i--) {
-			ans = max(ans, Next[i][j]);
 			if (Next[i][j]) continue;
 			for (int k = i - 1; k >= 1; k--) {
 				if (Next[k][j]) {
@@ -56,11 +90,16 @@ void DFS(int lev, vector<vector<int>> Now) {
 	}
 
 	DFS(lev + 1, Next);
+	//printf("☆★ level : %d ☆★\n", lev);
+	//print(Now);
+	//printf("에서 아래로 밀기 :: ");
+	//print(Next);
 	Next.clear();
 	//left-----------------------------------------------------------------
 		// merge : object 1개가 target 1잡고, 합치기!, target은 0이된다.
 	Next = Now;
 
+	/*
 	for (int i = 1; i <= N; i++) {
 		int object = -1; // merge할 object가 없다.
 		for (int j = 1; j <= N; j++) {
@@ -79,10 +118,24 @@ void DFS(int lev, vector<vector<int>> Now) {
 			}
 		}
 	}
+	*/
+	// left - merge
+	for (int i = 1; i <= N; i++) {
+		bool IsPushed[MAX] = { 0 };
+		for (int j = 1; j <= N; j++) {
+			if (Next[i][j] == 0 || IsPushed[j]) continue;
+			for (int k = j + 1; k <= N; k++) {
+				if (!Next[i][k] || IsPushed[k] || IsPushed[j]) continue;
+				if (Next[i][j] != Next[i][k]) break;
+				Next[i][j] += Next[i][k];
+				Next[i][k] = 0;
+				IsPushed[k] = IsPushed[j] = true;
+			}
+		}
+	}
 	// left - mv : 0을 메꿔서 땡긴다!, 0을 잡고 다음 tartget을 여기까지 땡긴다!
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			ans = max(ans, Next[i][j]);
 			if (Next[i][j]) continue;
 			for (int k = j + 1; k <= N; k++) {
 				if (Next[i][k]) {
@@ -95,25 +148,40 @@ void DFS(int lev, vector<vector<int>> Now) {
 	}
 
 	DFS(lev + 1, Next);
+	//printf("☆★ level : %d ☆★\n", lev);
+	//print(Now);
+	//printf("에서 왼쪽으로 밀기 :: ");
+	//print(Next);
 	Next.clear();
 	//up-----------------------------------------------------------------
 	Next = Now;
 
+	/*
 	for (int j = 1; j <= N; j++) {
-		int object = -1; // merge할 object가 없다.
+		bool IsPushed[MAX] = {0};
+		for (int i = N; i >= 1; i--) {
+			if (Next[i][j] == 0 || IsPushed[i]) continue;
+			for (int k = i - 1; k >= 1; k--) {
+				if (!Next[k][j] || IsPushed[k]) continue;
+				if (Next[i][j] != Next[k][j]) break;
+				Next[i][j] += Next[k][j];
+				Next[k][j] = 0;
+				IsPushed[k] = IsPushed[i] = true;
+			}
+		}
+	}
+	*/
+	// up-merge : 
+	for (int j = 1; j <= N; j++) {
+		bool IsPushed[MAX] = { 0 };
 		for (int i = 1; i <= N; i++) {
-			if (Next[i][j]) {
-				if (object == -1)
-					object = i; // merge할 object를 지금 찾았다.
-				else if (Next[object][j] == Next[i][j]) {
-					Next[object][j] += Next[i][j];
-					Next[i][j] = 0;
-					object = -1;
-				}
-				else {
-					object = -1;
-					i--;
-				}
+			if (Next[i][j] == 0 || IsPushed[i]) continue;
+			for (int k = i + 1; k <= N; k++) {
+				if (!Next[k][j] || IsPushed[k] || IsPushed[i]) continue;
+				if (Next[i][j] != Next[k][j]) break;
+				Next[i][j] += Next[k][j];
+				Next[k][j] = 0;
+				IsPushed[k] = IsPushed[i] = true;
 			}
 		}
 	}
@@ -121,7 +189,6 @@ void DFS(int lev, vector<vector<int>> Now) {
 	// up-mv : 0을 메꿔서 땡긴다!, 0을 잡고 다음 tartget을 여기까지 땡긴다!
 	for (int j = 1; j <= N; j++) {
 		for (int i = 1; i <= N; i++) {
-			ans = max(ans, Next[i][j]);
 			if (Next[i][j]) continue;
 			for (int k = i + 1; k <= N; k++) {
 				if (Next[k][j]) {
@@ -134,32 +201,58 @@ void DFS(int lev, vector<vector<int>> Now) {
 	}
 
 	DFS(lev + 1, Next);
+	//printf("☆★ level : %d ☆★\n", lev);
+	//print(Now);
+	//printf("에서 위로 밀기 :: ");
+	//print(Next);
 	Next.clear();
 	//right---------------------------------------------------------------
 	Next = Now;
+
+	/*
 	for (int i = 1; i <= N; i++) {
-		int object = -1; // merge할 object가 없다.
-		for (int j = N; j >= 1; j--) {
-			if (Next[i][j]) {
-				if (object == -1)
-					object = j; // merge할 object를 지금 찾았다.
-				else if (Next[i][object] == Next[i][j]) {
-					Next[i][object] += Next[i][j];
-					Next[i][j] = 0;
-					object = -1;
-				}
-				else {
-					object = -1;
-					j++;
-				}
+		bool IsPushed[MAX] = { 0 };
+		for (int j = 1; j <= N; j++) {
+			if (Next[i][j] == 0 || IsPushed[j]) continue;
+			for (int k = j + 1; k <= N; k++) {
+				if (!Next[i][k] || IsPushed[k]) continue;
+				if (Next[i][j] != Next[i][k]) break;
+				Next[i][j] += Next[i][k];
+				Next[i][k] = 0;
+				IsPushed[k] = IsPushed[j] = true;
 			}
 		}
 	}
+	*/
+	// right - merge
+	for (int i = 1; i <= N; i++) {
+		bool IsPushed[MAX] = { 0 };
+		for (int j = N; j >= 1; j--) {
+			if (Next[i][j] == 0 || IsPushed[j]) continue;
+			for (int k = j - 1; k >= 1; k--) {
+				if (!Next[i][k] || IsPushed[k] || IsPushed[j]) continue;
+				if (Next[i][j] != Next[i][k]) break;
+				Next[i][j] += Next[i][k];
+				Next[i][k] = 0;
+				IsPushed[k] = IsPushed[j] = true;
+			}
+		}
+	}
+	/*for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (Next[i][j] == 0) continue;
+			for (int k = j + 1; k <= N; k++) {
+				if (!Next[i][k]) continue;
+				if (Next[i][j] != Next[i][k]) break;
+				Next[i][j] += Next[i][k];
+				Next[i][k] = 0;
+			}
+		}
+	}*/
 
 	// right-mv : 0을 메꿔서 땡긴다!, 0을 잡고 다음 tartget을 여기까지 땡긴다!
 	for (int i = 1; i <= N; i++) {
 		for (int j = N; j >= 1; j--) {
-			ans = max(ans, Next[i][j]);
 			if (Next[i][j]) continue;
 			for (int k = j - 1; k >= 1; k--) {
 				if (Next[i][k]) {
@@ -171,6 +264,10 @@ void DFS(int lev, vector<vector<int>> Now) {
 		}
 	}
 	DFS(lev + 1, Next);
+	//printf("☆★ level : %d ☆★\n", lev);
+	//print(Now);
+	//printf("에서 오른쪽으로 밀기 :: ");
+	//print(Next);
 	Next.clear();
 }
 int main() {
--- exercise/BOJ/14500.cpp
+++ exercise/BOJ/14500.cpp
@@ -2,17 +2,16 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-const int MAX = 550;
+const int MAX = 600;
 const int LEFT = 6;
 int N, M, hei, wid, bn, sn, ans, board[MAX][MAX];
 int tboard[MAX][MAX];
 
 void turn() {
 
-
-	for (int j = 0; j < hei + LEFT; j++) {
-		for (int k = 0; k < wid + LEFT; k++) {
-			tboard[k][j] = board[j][wid + LEFT - 1 - k];
+	for (int j = 0; j < bn + LEFT; j++) {
+		for (int k = 0; k < bn + LEFT; k++) {
+			tboard[k][j] = board[j][bn + LEFT - 1 - k];
 		}
 	}
 
@@ -20,33 +19,23 @@ void turn() {
 	hei = wid;
 	wid = tmp;
 
-	for (int i = 0; i < hei + LEFT; i++)
-		for (int j = 0; j < wid + LEFT; j++)
+	for (int i = 0; i < bn + LEFT; i++)
+		for (int j = 0; j < bn + LEFT; j++)
 			board[i][j] = tboard[i][j];
-
 }
 
 void rev() {
-
-	for (int i = 0; i < hei + LEFT; i++) {
-		for (int j = 0; j < wid + LEFT; j++) {
+	for (int i = 0; i < bn + LEFT; i++) {
+		for (int j = 0; j < bn + LEFT; j++) {
 			tboard[i][j] = board[i][bn + LEFT - 1 - j];
 		}
 	}
 
-	for (int i = 0; i < hei + LEFT; i++)
-		for (int j = 0; j < wid + LEFT; j++)
+	for (int i = 0; i < bn + LEFT; i++)
+		for (int j = 0; j < bn + LEFT; j++)
 			board[i][j] = tboard[i][j];
 }
 
-void print() {
-	for (int i = 0; i < bn + LEFT; i++) {
-		for (int j = 0; j < bn + LEFT; j++) {
-			printf("%4d", board[i][j]);
-		}
-		printf("\n");
-	}
-}
 
 
 int main() {
@@ -96,8 +85,8 @@ int main() {
 	for (int t = 0; t < 4; t++) {
 		for (int r = 0; r < 2; r++) {
 			for (int k = 0; k < 5; k++) {
-				for (int i = 0; i < hei + 6; i++) {
-					for (int j = 0; j < wid + 6; j++) {
+				for (int i = 0; i < bn + LEFT; i++) {
+					for (int j = 0; j < bn + LEFT; j++) {
 						int tmp = 0;
 						int cnt = 0;
 						for (int yy = 0; yy < 4; yy++) {
@@ -113,14 +102,11 @@ int main() {
 					}
 				}
 			}
-			rev();
-			printf("reverse ! \n");
-			print();
+			rev(); // 리버스 뒤집기 한번하고, 되돌려두기
 		}
-		printf("turn ! \n");
-		turn();
-		print();
+		turn(); // 반시계방향으로 회전.
 	}
 
 	printf("%d\n", ans);
+	return 0;
 }
